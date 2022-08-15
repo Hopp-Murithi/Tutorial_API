de6692779a87f59f7f0e40635e7e1c6c8f31c17d
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-15 22:45:58 +0300

    create route handler for health check and load onto server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //Defines logic about the server
 const express = require('express')
+const health = require('./routes/healthChecker.routes')
 const app = express();
+
 app.use(express.json());
+
+app.use('/health', health)
 
 
 const port = process.env.PORT || 3000
