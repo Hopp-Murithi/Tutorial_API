58e9a448dfdd6153dac1a9ef653eedbae20c2372
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-15 16:13:21 +0300

    Create server healthcheck controller logic

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 //Defines logic about the server
 const express = require('express')
 const app = express();
+app.use(express.json());
 
-app.listen(3000)
+
+const port = process.env.PORT || 3000
+app.listen(3000, () => {
+    console.log(`Listening on port ${port}`)
+})
