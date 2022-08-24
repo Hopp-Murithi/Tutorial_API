38969888babb1fa14f6ea7a5bbca4cd75cb62443
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-24 14:37:10 +0300

    Define user Schema and Create user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,34 @@
 //defines schema model for creating users
+const mongoose = require('mongoose');
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5
+    }
+});
+
+const User = mongoose.model('User', userSchema);
