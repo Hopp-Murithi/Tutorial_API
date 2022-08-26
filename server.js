5298f2566e0ab99a168e541a6011c51bb0e47c8b
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-26 09:11:37 +0300

    load new user route  onto root file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const health = require('./routes/healthChecker.routes')
+const users = require('./routes/users.routes')
 const app = express();
 
 
@@ -11,7 +12,8 @@ require('./config/db')();
 
 app.use(express.json());
 
-app.use('/health', health)
+app.use('/health', health);
+app.use('/users', users);
 
 
 const port = process.env.PORT || 3000
