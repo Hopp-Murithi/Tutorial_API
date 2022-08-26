49dd6e2ba705247231d1162ac73a199dc118e73c
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-26 09:05:15 +0300

    Create route handler for creating new users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,1 +1,8 @@
 //Defines endpoints for handling creation of new users
+
+const express = require('express');
+const router = express.Router();
+const { users } = require('../controllers/users');
+
+
+router.route('/users').post(users);
