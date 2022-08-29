68959f59567ca935c52060927ae4da8c41c8088a
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-29 11:41:29 +0300

    import startup configs correctly

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 //Defines logic about the server
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv');
-const health = require('./routes/healthChecker.routes')
-const users = require('./routes/users.routes')
 const app = express();
+
 
 
 dotenv.config();
 
 require('./config/db')();
-
-app.use(express.json());
-
-app.use('/health', health);
-app.use('/users', users);
+require('./config/server.config')(app)
 
 
 const port = process.env.PORT || 3000
