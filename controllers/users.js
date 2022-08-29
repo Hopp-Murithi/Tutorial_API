c26182ba9d38677de0156a68edd77c1c581e9648
Hopp-Murithi <hoppmurithi@gmail.com>
2022-08-29 11:42:19 +0300

    Refactor code

--- controllers/users.js
+++ controllers/users.js
@@ -9,14 +9,14 @@ const bcrypt = require('bcrypt');
 let mailer = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.USER_MAIL,
-        pass: process.env.USER_USER_SECRET
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 });
 
 let details = {
-    from: process.env.USER_MAIL,
-    to: user.email,
+    from: process.env.SMTP_USER,
+    to: process.env.SMTP_RECIPIENT,
     subject: `Welcome on board`,
     text: 'Hi there and welcome'
 
@@ -31,31 +31,40 @@ mailer.sendMail(details, (err, info) => {
 
 
 
-exports.users('', async(req, res) => {
+module.exports.users = async(req, res) => {
     try {
         const email = await user.findOne({ email: req.body.email });
+        const firstName = await user.find({ firstName: req.body.firstName });
+        const lastName = await user.find({ lastName: req.body.lastName });
+        const password = await user.find({ password: req.body.password });
+        const confirmPassword = await user.find({ confirmPassword: req.body.confirmPassword });
+        const phoneNumber = await user.find({ phoneNumber: req.body.phoneNumber });
+
+        if (!email) return res.send('Email is required')
+
         const filterEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if (!filterEmail.test(email.value)) return
-        res.send('Please provide a valid email address');;
+        res.send('Please provide a valid email address');
         if (email) {
             res.send('Email already exists');
         };
 
-        const firstName = await user.find({ firstName: req.body.firstName });
+        if (!firstName) return res.send('First Name is required');
         if (firstName.length < 3 || firstName.length > 20) return
         res.send('First name must be at least 3 characters and less than 20 characters');;
-        const lastName = await user.find({ lastName: req.body.lastName });
+
+        if (!lastName) return res.send('Last Name is required');
         if (lastName.length < 3 || lastName.length > 20) return
         res.send('Last name must be at least 3 characters and less than 20 characters')
 
-        const password = await user.find({ password: req.body.password });
-        const confirmPassword = await user.find({ confirmPassword: req.body.confirmPassword });
+        if (!password) return res.send('PassWord is required');
         const filterPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{5,15}$/;
         if (!filterPassword.test(password.value)) return
         res.send('Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character');
+        if (!confirmPassword) return res.send(' Please confirm password');
         if (password.value !== confirmPassword.value) return res.send('Passwords do not match');
 
-        const phoneNumber = await user.find({ phoneNumber: req.body.phoneNumber });
+        if (!phoneNumber) return res.send('Phone Number is required');
         phone(phoneNumber.value);
 
         let User = new user(_.pick(req.body, ['firstName', 'lastName', 'email', 'phoneNumber', 'password']));
@@ -68,8 +77,9 @@ exports.users('', async(req, res) => {
 
 
     } catch (err) {
-        res.status(500).send('Something went wrong', err);
+        res.status(500).send('Something went wrong');
+        console.warn(err);
     }
 
 
-})
+}
