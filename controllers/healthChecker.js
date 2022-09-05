b69f74b43a53a2e906f65f20d0c751b074acae9d
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-05 10:55:23 +0300

    Delete health checker file

--- controllers/healthChecker.js
+++
@@ -1,8 +0,0 @@
-//controller logic for server health check
-module.exports.health = async function(req, res, _next) {
-    try {
-        res.status(200).json({ message: "Server is running" })
-    } catch (err) {
-        res.status(503).json({ devError: err.message, message: "Failed" })
-    }
-};
