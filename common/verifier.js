f69d57f49beaf77cda50a9c1b1ff7faeed3520d1
Hopp-Murithi <hoppmurithi@gmail.com>
2022-09-06 14:19:15 +0300

    Add password control logic

--- common/verifier.js
+++ common/verifier.js
@@ -34,6 +34,21 @@ module.exports = {
         if (lastName.length < 3 || lastName.length > 20) {
             return res.status(411).json({ message: 'length must be between 3 and 20 characters' });
         }
+    },
+    verifyPassword: (password, confirmPassword) => {
+        if (!password) {
+            return res.status(404).json({ message: 'Password field is required' })
+        }
+        if (password.length < 5) {
+            return res.status(411).json({ message: 'Password is too short' })
+        }
+        let passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{5,15}$/;
+        if (!password.match(passRegex)) {
+            return res.status(400).json({ message: 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character' });
+        }
+        if (password !== confirmPassword) {
+            return res.status(417).json({ message: 'passwords do not match' })
+        }
     }
 
 }
