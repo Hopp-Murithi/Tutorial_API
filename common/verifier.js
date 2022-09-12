898efa6b1fa038c2f48e16fe7c1dcdb4fd55ab1c
Hopp-Murithi <hoppmurithi@gmail.com>
2022-09-12 11:25:42 +0300

    remove undefined res in validation return statements

--- common/verifier.js
+++ common/verifier.js
@@ -5,58 +5,111 @@ const user = require('../models/user');
 module.exports = {
     verifyEmail: async(email) => {
         if (!email) {
-            return res.status(404).json({ message: 'Email is required' })
+            return {
+                status: 404,
+                message: 'Email is required'
+
+            }
         }
         let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (!email.match(regex)) {
-            return res.status(400).json({
+            return {
+                status: 400,
                 message: 'invalid email address'
-            })
+
+            }
         }
         let User = await user.findOne({ email });
         if (User) {
-            return res.status(400).json({ message: 'User already exists' })
+            return {
+                status: 400,
+                message: 'User already exists'
+
+            }
         }
+
 
     },
     verifyFirstName: (firstName) => {
         if (!firstName) {
-            return res.status(404).json({ message: 'First name is required' });
+            return {
+                status: 404,
+                message: 'First name is required'
+            }
         }
         if (firstName.length < 3 || firstName.length > 20) {
-            return res.status(411).json({ message: 'length must be between 3 and 20 characters' });
+            return {
+                status: 411,
+                message: 'length must be between 3 and 20 characters'
+
+            }
+
         }
     },
     verifyLastName: (lastName) => {
         if (!lastName) {
-            return res.status(404).json({ message: 'First name is required' });
+            return {
+                status: 404,
+                message: 'First name is required'
+
+            }
+
         }
         if (lastName.length < 3 || lastName.length > 20) {
-            return res.status(411).json({ message: 'length must be between 3 and 20 characters' });
+            return {
+                status: 411,
+                message: 'length must be between 3 and 20 characters'
+
+            }
+
         }
     },
     verifyPassword: (password, confirmPassword) => {
         if (!password) {
-            return res.status(404).json({ message: 'Password field is required' })
+            return {
+                status: 404,
+                message: 'Password field is required'
+
+            }
         }
         if (password.length < 5) {
-            return res.status(411).json({ message: 'Password is too short' })
+            return {
+                status: 411,
+                message: 'Password is too short'
+
+            }
+
         }
         let passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{5,15}$/;
         if (!password.match(passRegex)) {
-            return res.status(400).json({ message: 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character' });
+            return {
+                status: 400,
+                message: 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character'
+            }
+
         }
         if (password !== confirmPassword) {
-            return res.status(417).json({ message: 'passwords do not match' })
+            return {
+                status: 417,
+                message: 'passwords do not match'
+
+            }
+
         }
     },
     verifyPhone: (phoneNumber) => {
         if (!phoneNumber) {
-            return res.status(404).json({ message: 'Phone number is required' })
+            return {
+                status: 404,
+                message: 'Phone number is required'
+            }
+
         }
         let phoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
         if (!phoneNumber.match(phoneRegex)) {
-            return res.status(400).json({ message: 'Please add a valid phone number' })
+            return { status: 400, message: 'Please add a valid phone number' }
+
+
         }
     }
 
