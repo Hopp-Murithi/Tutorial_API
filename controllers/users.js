3a341432868491dd22a0649be5b1e9d914475393
Hopp-Murithi <hoppmurithi@gmail.com>
2022-09-12 11:22:45 +0300

    modify phone number verification method

--- controllers/users.js
+++ controllers/users.js
@@ -2,42 +2,21 @@
 const user = require('../models/user');
 const nodemailer = require('nodemailer');
 const verifier = require('../common/verifier')
-const _ = require('lodash');
 const bcrypt = require('bcrypt');
 
 
-let mailer = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-});
-
-let details = {
-    from: process.env.SMTP_USER,
-    to: process.env.SMTP_RECIPIENT,
-    subject: `Welcome on board`,
-    text: 'Hi there and welcome'
-
-}
-mailer.sendMail(details, (err, info) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('email sent', info);
-    }
-})
 
 
 
 module.exports.users = async(req, res) => {
     try {
+        const { email, firstName, lastName, password, confirmPassword, phoneNumber } = req.body
+
         verifier.verifyEmail(email);
         verifier.verifyFirstName(firstName);
         verifier.verifyLastName(lastName);
         verifier.verifyPassword(password, confirmPassword);
-        verifier.phoneNumber(phoneNumber);
+        verifier.verifyPhone(phoneNumber);
 
 
         let User = new user({
@@ -53,6 +32,29 @@ module.exports.users = async(req, res) => {
             return user.password = await bcrypt.hash(user.password, salt);
         });
         await User.save();
+        let mailer = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        });
+
+        let details = {
+            from: process.env.SMTP_USER,
+            to: user.password,
+            subject: `Welcome on board`,
+            text: 'Hi there and welcome'
+
+        }
+        mailer.sendMail(details, (err, info) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('email sent', info);
+            }
+        })
+
 
 
     } catch (err) {
