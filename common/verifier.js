7f606696a2888880bbcc04c6d13423f721f0829d
Hopp-Murithi <hoppmurithi@gmail.com>
2022-09-21 16:36:20 +0300

    Create validation logic using else if statements

--- common/verifier.js
+++ common/verifier.js
@@ -2,57 +2,62 @@ const user = require('../models/user')
 
 module.exports = {
     verifyEmail: async(email) => {
+
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const emailExists = await user.findOne({ email });
+
         if (!email) {
-            return { status: (404), message: 'Email is required' }
-        }
-        let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (!email.match(regex)) {
-            return { status: (400), message: 'invalid email address' }
-        }
-        let User = await user.findOne({ email });
-        if (User) {
-            return { status: (400), message: 'User already exists' }
+            return { message: 'Email is required' }
+        } else if (!emailRegex.test(email)) {
+            return { message: "Invalid email" };
+        } else if (emailExists) {
+            return { message: "Email already exists" };
+        } else {
+            return true;
         }
 
     },
     verifyFirstName: (firstName) => {
         if (!firstName) {
-            return { status: (404), message: 'First name is required' }
-        }
-        if (firstName.length < 3 || firstName.length > 20) {
-            return { status: (400), message: 'length must be between 3 and 20 characters' }
+            return { message: 'First name is required' }
+        } else if (firstName.length < 3 || firstName.length > 20) {
+            return { message: 'length must be between 3 and 20 characters' }
+        } else {
+            return true;
         }
     },
     verifyLastName: (lastName) => {
         if (!lastName) {
-            return { status: (404), message: 'Last name is required' }
-        }
-        if (lastName.length < 3 || lastName.length > 20) {
-            return { status: (400), message: 'length must be between 3 and 20 characters' }
+            return { message: 'Last name is required' }
+        } else if (lastName.length < 3 || lastName.length > 20) {
+            return { message: 'length must be between 3 and 20 characters' }
+        } else {
+            return true;
         }
     },
     verifyPassword: (password, confirmPassword) => {
-        if (!password) {
-            return { status: (404), message: 'Password field is required' }
-        }
-        if (password.length < 5) {
-            return { status: (400), message: 'Password is too short' }
-        }
+
         let passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{5,15}$/;
-        if (!password.match(passRegex)) {
-            return { status: (400), message: 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character' }
-        }
-        if (password !== confirmPassword) {
-            return { status: (400), message: 'passwords do not match' }
+        if (!password) {
+            return { message: 'Password field is required' }
+        } else if (password.length < 5) {
+            return { message: 'Password is too short' }
+        } else if (!passRegex.test(password)) {
+            return { message: 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character' }
+        } else if (password !== confirmPassword) {
+            return { message: 'passwords do not match' }
+        } else {
+            return true;
         }
     },
     verifyPhone: (phoneNumber) => {
-        if (!phoneNumber) {
-            return { status: (404), message: 'Phone number is required' }
-        }
         let phoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
-        if (!phoneNumber.match(phoneRegex)) {
-            return { status: (400), message: 'Please add a valid phone number' }
+        if (!phoneNumber) {
+            return { message: 'Phone number is required' }
+        } else if (!phoneRegex.test(phoneNumber)) {
+            return { message: 'Please add a valid phone number' }
+        } else {
+            return true;
         }
     }
 
