cf748f66b6d695baaa5a3a030aaad90a66510c1c
Hopp-Murithi <hoppmurithi@gmail.com>
2022-10-11 06:37:05 +0300

    Create console log for new user without the confirm password field in the database

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 //Defines control logic for creating new users
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
-const { verifyEmail, verifyFirstName, verifyLastName, verifyPassword, verifyPhone } = require('../common/verifier')
+const { verifyEmail, verifyFirstName, verifyLastName, verifyPassword, verifyPhone } = require('../common/verifier');
 const bcrypt = require('bcrypt');
 
 module.exports.users = async(req, res) => {
@@ -36,8 +36,9 @@ module.exports.users = async(req, res) => {
 
         const salt = await bcrypt.genSalt(Number(10));
         const hashedPassword = await bcrypt.hash(password, salt);
-        await new User({...req.body, password: hashedPassword }).save();
-
+        const regs = await new User({...req.body, password: hashedPassword }).save();
+        regs.confirmPassword = null;
+        console.log(regs)
         let mailer = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -49,7 +50,7 @@ module.exports.users = async(req, res) => {
         let details = {
             from: process.env.SMTP_USER,
             to: email,
-            subject: `Welcome on board ${firstName}`,
+            subject: `Welcome on board ${firstName} ${lastName}`,
             text: 'Hi there and welcome to HOPP Softwares Inc. We are glad to have you on board. We hope you enjoy your stay with us.'
 
         }
