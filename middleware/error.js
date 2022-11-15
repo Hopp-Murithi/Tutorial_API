ea4e5661436ae40640ba62e77e4777b849921baf
Hopp Murithi <hoppmurithi@gmail.com>
2022-11-15 10:08:09 +0300

    Create and export error handling functionality

---
+++ middleware/error.js
@@ -0,0 +1,18 @@
+module.exports = class ApiError  {
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  static badRequest(message) {
+    return new ApiError(400, message);
+  }
+
+  static internalServerError(message) {
+    return new ApiError(500, message);
+  }
+
+  static forbidden(message) {
+    return new ApiError(403, message);
+  }
+}
