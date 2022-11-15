02f93cc1deb7937ffd1beca532ac14be953e7e03
Hopp Murithi <hoppmurithi@gmail.com>
2022-11-15 06:59:04 +0300

    Load login authorization route onto server

--- config/server.config.js
+++ config/server.config.js
@@ -1,13 +1,17 @@
 //Defines all routes to load onto server(code refactoring)
 const health = require('../routes/health.routes');
 const users = require('../routes/users.routes');
+const auth = require('../routes/auth.routes');
+
 const express = require('express');
 
 
 module.exports = function(app) {
     app.use(express.json());
-    app.use('/api/health/', health);
+    app.use('/api/health', health);
     app.use('/api/users', users);
+    app.use('/api/auth', auth);
+    
 
 
 }
