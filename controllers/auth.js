9defbdd5378766daefdaab17188c6157c57b2cb9
Hopp Murithi <hoppmurithi@gmail.com>
2023-01-26 22:16:49 +0300

    Add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 //Defines control logic for logging in/out users
 const User=require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const ApiError = require('../middleware/error') //This is the error handler middleware
 
 module.exports.auth = async(req, res,next) => {
@@ -11,6 +13,10 @@ module.exports.auth = async(req, res,next) => {
         const validPassword = await bcrypt.compare(password, user.password);
         if(!validPassword) return res.status(400).json({message: 'Invalid email or password'});
         res.status(200).json({message: 'Login successful'});
+
+const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+res.json({accessToken:accessToken})
+
     }
     catch(err){
 next(ApiError.forbidden(err.message))
